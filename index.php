0a9fbc780b4f515d2c9f531d4fd587ba051f98a5
Remembers5 <35206632+Remembers5@users.noreply.github.com>
2018-03-15 09:13:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
-<?php 
+<?php
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 require_once('./LINEBotTiny.php');
-$channelAccessToken = 'RlvjJ75b/Y6pZvkRubi439aOHnbo4DLIby0emSb7yHg0rvijrUwLIotURErm06Zo7/YO+xFacGJd2xwZfD+dOCmKguVOo8ZBBhhwBP4EW6lrfmbxw6ErquSTObl7fXrnNPARYpZpCiqWxzqp49xzOwdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '09be4e00415d8079d87f1a834165bb2b';
+$channelAccessToken = '<your channel access token>';
+$channelSecret = '<your channel secret>';
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
@@ -9,54 +24,15 @@ foreach ($client->parseEvents() as $event) {
             $message = $event['message'];
             switch ($message['type']) {
                 case 'text':
-                    if($message['text']=="確認"){
-                        $client->replyMessage(array(
-                            'replyToken' => $event['replyToken'],
-                            'messages' => array(
-                                array(
-                                    'type' => 'template',
-                                    'altText' => 'this is a confirm template',
-                                    'template'=> array( 'type'=>'confirm',
-                                                        'text'=>'Are you sure?',
-                                                        'actions'=>array(array('type'=>"message","label"=>"Yes","text"=>"yes"),array('type'=>"message","label"=>"No","text"=>"no"))
-                                        )
-                                )
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text']
                             )
-                        ));
-                    }elseif($message['text']=="按鈕") {
-                        $client->replyMessage(array(
-                            'replyToken' => $event['replyToken'],
-                            'messages' => array(
-                                array(
-                                    'type' => 'template',
-                                    'altText' => 'This is a buttons template',
-                                    'template'=> array( 'type'=>'buttons',
-                                                        'thumbnailImageUrl'=>'https://www.pkstep.com/wp-content/uploads/2015/09/LINE@tech1.png',
-                                                        'imageAspectRatio'=>'rectangle',
-                                                        'imageSize'=>'cover',
-                                                        'imageBackgroundColor'=>'#FFFFFF',
-                                                        'title'=>'Menu',
-                                                        'text'=>'Please select',
-                                                        'defaultAction'=>array('type'=>'uri','label'=>'View detail','uri'=>'https://www.google.com.tw'),
-                                                        'actions'=>array(array('type'=>'postback','label'=>'Buy','data'=>'action=buy&itemid=123'),
-                                                                        array('type'=>'message','label'=>'Add to cart','text'=>'Add to cart'),
-                                                                        array('type'=>'uri','label'=>'View detail','uri'=>'https://www.google.com.tw')
-                                                                    )
-                                        )
-                                )
-                            )
-                        ));
-                    }else{
-                        $client->replyMessage(array(
-                            'replyToken' => $event['replyToken'],
-                            'messages' => array(
-                                array(
-                                    'type' => 'text',
-                                    'text' => $message['text']
-                                )
-                            )
-                        ));
-
+                        )
+                    ));
                     break;
                 default:
                     error_log("Unsupporeted message type: " . $message['type']);
