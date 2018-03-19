e0a5005ad20117a2ecf7bdcca86fa23fe8597ba5
Remembers5 <35206632+Remembers5@users.noreply.github.com>
2018-03-19 13:32:40 +0800

    Update index.php

--- assets/images/example/1040/index.php
+++ assets/images/example/1040/index.php
@@ -1,5 +1,4 @@
 <?php
-ini_set('php_gd2.dll', 1);
 $path = "../example.jpg"; // 圖片所在網址
 $type = getimagesize($path); // 取得圖片資訊
 switch($type[2]){ // 判斷圖片的類型
@@ -8,13 +7,27 @@ switch($type[2]){ // 判斷圖片的類型
     case 3 : $img_type = "png"; break;  
 }
 header("Content-type: image/" . $img_type); // 設定圖檔格式
-
+$percent = 1 / (1040 / 1040); // 縮略圖比例 公式：1 / (原始圖大小 / 調整後大小)
+// 縮略圖尺寸
+list($width, $height) = getimagesize($path);
+$newwidth = round($width * $percent);
+$newheight = round($height * $percent);
+$dst_im = imagecreatetruecolor($newwidth, $newheight);
 if ($img_type == "jpeg"){
     $src_im = imagecreatefromjpeg($path);
-
-    imagejpeg($src_im); //輸出調整大小後的圖像
+    imagecopyresized($dst_im, $src_im, 0, 0, 0, 0, $newwidth, $newheight, $width, $height); // 調整大小
+    imagejpeg($dst_im); //輸出調整大小後的圖像
 }
-
-
+if ($img_type == "png"){
+    $src_im = imagecreatefrompng($path);
+    imagecopyresized($dst_im, $src_im, 0, 0, 0, 0, $newwidth, $newheight, $width, $height); // 調整大小
+    imagepng($dst_im); //輸出調整大小後的圖像
+}
+if ($img_type == "gif"){
+    $src_im = imagecreatefromgif($path);
+    imagecopyresized($dst_im, $src_im, 0, 0, 0, 0, $newwidth, $newheight, $width, $height); // 調整大小
+    imagegif($dst_im); //輸出調整大小後的圖像
+}
+imagedestroy($dst_im);
 imagedestroy($src_im);
 ?>
