b22fb5836247f3ac66926e7749bbf2011269a18e
Remembers5 <35206632+Remembers5@users.noreply.github.com>
2018-03-19 11:35:14 +0800

    Update index.php

--- assets/images/example/1040/index.php
+++ assets/images/example/1040/index.php
@@ -13,6 +13,11 @@ $percent = 1 / (1040 / 1040); // 縮略圖比例 公式：1 / (原始圖大小 /
 list($width, $height) = getimagesize($path);
 $newwidth = round($width * $percent);
 $newheight = round($height * $percent);
+$dst_im = imagecreatetruecolor($newwidth, $newheight);
+if ($img_type == "jpeg"){
+    $src_im = imagecreatefromjpeg($path);
+    imagecopyresized($dst_im, $src_im, 0, 0, 0, 0, $newwidth, $newheight, $width, $height); // 調整大小
+    imagejpeg($dst_im); //輸出調整大小後的圖像
+}
 
 print_r($img_type);
-
