142e5bb8ce4be7a14a88566bd81b7ef083b56d1d
Remembers5 <35206632+Remembers5@users.noreply.github.com>
2018-03-19 16:22:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,43 +9,151 @@ foreach ($client->parseEvents() as $event) {
             $message = $event['message'];
             switch ($message['type']) {
                 case 'text':
-    $client->replyMessage(array(
-        'replyToken' => $event['replyToken'],
-        'messages' => array(
-            array(
-                'type' => 'imagemap', // 訊息類型 (圖片地圖)
-                'baseUrl' => 'https://tidal-velocity-196003.appspot.com', // 圖片網址 (可調整大小 240px, 300px, 460px, 700px, 1040px)
-                'altText' => 'Example imagemap', // 替代文字
-                'baseSize' => array(
-                    'height' => 1040, // 圖片寬
-                    'width' => 1040 // 圖片高
-                ),
-                'actions' => array(
-                    array(
-                        'type' => 'uri', // 類型 (網址)
-                        'linkUri' => 'https://github.com/GoneTone/line-example-bot-php', // 連結網址
-                        'area' => array(
-                            'x' => 0, // 點擊位置 X 軸
-                            'y' => 0, // 點擊位置 Y 軸
-                            'width' => 520, // 點擊範圍寬度
-                            'height' => 1040 // 點擊範圍高度
-                        )
-                    ),
-                    array(
-                        'type' => 'message', // 類型 (用戶發送訊息)
-                        'text' => 'Hello', // 發送訊息
-                        'area' => array(
-                            'x' => 520, // 點擊位置 X 軸
-                            'y' => 0, // 點擊位置 Y 軸
-                            'width' => 520, // 點擊範圍寬度
-                            'height' => 1040 // 點擊範圍高度
-                        )
-                    )
-                )
-            )
-        )
-    ));
-
+                    if($message['text']=="Confirm"){
+                        $client->replyMessage(array(
+                            'replyToken' => $event['replyToken'],
+                            'messages' => array(
+                                array(
+                                    'type' => 'template',
+                                    'altText' => 'this is a confirm template',
+                                    'template'=> array( 'type'=>'confirm',
+                                                        'text'=>'Are you sure?',
+                                                        'actions'=>array(array('type'=>"message","label"=>"Yes","text"=>"yes"),array('type'=>"message","label"=>"No","text"=>"no"))
+                                        )
+                                )
+                            )
+                        ));
+                    }elseif($message['text']=="Buttons") {
+                        $client->replyMessage(array(
+                            'replyToken' => $event['replyToken'],
+                            'messages' => array(
+                                array(
+                                    'type' => 'template',
+                                    'altText' => 'This is a buttons template',
+                                    'template'=> array( 'type'=>'buttons',
+                                                        'thumbnailImageUrl'=>'https://www.pkstep.com/wp-content/uploads/2015/09/LINE@tech1.png',
+                                                        'imageAspectRatio'=>'rectangle',
+                                                        'imageSize'=>'cover',
+                                                        'imageBackgroundColor'=>'#FFFFFF',
+                                                        'title'=>'Menu',
+                                                        'text'=>'Please select',
+                                                        'defaultAction'=>array('type'=>'uri','label'=>'View detail','uri'=>'https://www.google.com.tw'),
+                                                        'actions'=>array(array('type'=>'postback','label'=>'php','data'=>'action=php&id=php'),
+                                                                        array('type'=>'message','label'=>'java','text'=>'java'),
+                                                                        array('type'=>'uri','label'=>'python','uri'=>'https://www.google.com.tw')
+                                                                    )
+                                        )
+                                )
+                            )
+                        ));
+                    }elseif ($message['text']=="Carousel") { 
+                        $client->replyMessage(array(
+                            'replyToken' => $event['replyToken'],
+                            'messages' => array(
+                                    array(  "type"=>"template",
+                                            "altText"=>"this is a carousel template",
+                                            "template"=>array(  "type"=>"carousel",
+                                                    "columns"=>array(array( "thumbnailImageUrl"=>"https://upload.wikimedia.org/wikipedia/zh/8/88/Java_logo.png",
+                                                                            "imageBackgroundColor"=>"#FFFFFF",
+                                                                            "title"=>"this is menu",
+                                                                            "text"=>"description",
+                                                                            "defaultAction"=>array( "type"=>"uri",
+                                                                                                    "label"=>"View detail",
+                                                                                                    "uri"=>"http://example.com/page/123"
+                                                                                                ),
+                                                                            "actions"=>array(array( "type"=>"postback", "label"=>"php", "data"=>"action=php&id=php"),
+                                                                                            array(  "type"=>"postback", "label"=>"java", "data"=>"action=java&id=java"),
+                                                                                            array(  "type"=>"uri", "label"=>"python", "uri"=>"http://example.com/page/111"),
+                                                                                        ),
+                                                                    ),array( "thumbnailImageUrl"=>"https://snowplowanalytics.com/assets/img/blog/2016/09/python-logo.png",
+                                                                            "imageBackgroundColor"=>"#000000",
+                                                                            "title"=>"this is menu",
+                                                                            "text"=>"description",
+                                                                            "defaultAction"=>array( "type"=>"uri",
+                                                                                                    "label"=>"View detail",
+                                                                                                    "uri"=>"http://example.com/page/222"
+                                                                                                ),
+                                                                            "actions"=>array(array( "type"=>"postback", "label"=>"python", "data"=>"action=python&id=python"),
+                                                                                            array(  "type"=>"postback", "label"=>"java", "data"=>"action=java&id=java"),
+                                                                                            array(  "type"=>"uri", "label"=>"php", "uri"=>"https://www.foolegg.com/wp-content/uploads/2012/06/php.png"),
+                                                                                        ),
+                                                                    )
+                                                            ),
+                                                    "imageAspectRatio"=>"rectangle",
+                                                    "imageSize"=>"cover"
+                                            )
+                                        )
+                            )
+                        ));
+                    }elseif ($message['text']=="Image carousel") {
+                        $client->replyMessage(array(
+                            'replyToken' => $event['replyToken'],
+                            'messages' => array(
+                                                array(
+                                                    'type'=>'template',
+                                                    'altText'=>'this is a image carousel template',
+                                                    'template'=>array(
+                                                           'type'=>'image_carousel',
+                                                           'columns'=>array(
+                                                                        array('imageUrl'=>'https://snowplowanalytics.com/assets/img/blog/2016/09/python-logo.png','action'=>array('type'=>'postback','label'=>'python','data'=>'action=python&id=python')),
+                                                                        array('imageUrl'=>'https://upload.wikimedia.org/wikipedia/zh/8/88/Java_logo.png','action'=>array('type'=>'message','label'=>'java','text'=>'java')),
+                                                                        array('imageUrl'=>'https://www.foolegg.com/wp-content/uploads/2012/06/php.png','action'=>array('type'=>'uri','label'=>'php','uri'=>'http://example.com/page/222'))
+                                                                )
+                                                    )
+                                                )
+                            )
+                        ));
+                    }elseif ($message['text']=="Imagemap") {
+                        $client->replyMessage(array(
+                            'replyToken' => $event['replyToken'],
+                            'messages' => array(
+                                array(
+                                    "type"=>"imagemap",
+                                    "baseUrl"=>"https://tidal-velocity-196003.appspot.com",
+                                    "altText"=>"This is an imagemap",
+                                    'baseSize' => array(
+                                        'height' => 1040, 
+                                        'width' => 1040 
+                                    ),
+                                    'actions' => array(
+                                        array(
+                                            'type' => 'uri', 
+                                            'linkUri' => 'https://github.com/GoneTone/line-example-bot-php', 
+                                            'area' => array(
+                                                'x' => 0, 
+                                                'y' => 0,
+                                                'width' => 520, 
+                                                'height' => 1040 
+                                            )
+                                        ),
+                                        array(
+                                            'type' => 'message', 
+                                            'text' => 'Hello', 
+                                            'area' => array(
+                                                'x' => 520, 
+                                                'y' => 0, 
+                                                'width' => 520, 
+                                                'height' => 1040 
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        ));
+                    }else{
+                        
+                        $client->replyMessage(array(
+                            'replyToken' => $event['replyToken'],
+                            'messages' => array(
+                                array(
+                                    'type' => 'text',
+                                    'text'=>$message['text']
+                                    //'text' => $event['source']['userId']
+                                )
+                            )
+                        ));
+                        
+                    }
                     break;
                 default:
                     error_log("Unsupporeted message type: " . $message['type']);
