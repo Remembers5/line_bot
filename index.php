8ba051098eaf646d6856286c80d582789d06b5ca
Remembers5 <35206632+Remembers5@users.noreply.github.com>
2018-03-26 16:29:48 +0800

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ foreach ($client->parseEvents() as $event) {
                                                                                                     "label"=>"View detail",
                                                                                                     "uri"=>"http://example.com/page/123"
                                                                                                 ),
-                                                                            "actions"=>array(array( "type"=>"postback", "label"=>"php","text": "php","data"=>"action=php&id=php"),
+                                                                            "actions"=>array(array( "type"=>"postback", "label"=>"php","text"=>"php","data"=>"action=php&id=php"),
                                                                                             array(  "type"=>"postback", "label"=>"java", "data"=>"action=java&id=java"),
                                                                                             array(  "type"=>"uri", "label"=>"python", "uri"=>"http://example.com/page/111"),
                                                                                         ),
