176d11e3a1d1d4cde5add3c9f680229df9f2fd83
Remembers5 <35206632+Remembers5@users.noreply.github.com>
2018-03-26 17:04:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -26,8 +26,7 @@ foreach ($client->parseEvents() as $event) {
                                                                                                     "uri"=>"http://example.com/page/123"
                                                                                                 ),
                                                                             "actions"=>array(array( "type"=>"postback", "label"=>"php","displayText"=>"php","data"=>"action=php&id=php"),
-                                                                                            array(  "type"=>"message", "label"=>"java", "text"=>"javaisgood"),
-                                                                                            array(  "type"=>"postback", "label"=>"python", "text"=>"pythonisgood")
+                                                                                            array(  "type"=>"message", "label"=>"java", "text"=>"javaisgood")
                                                                                         ),
                                                                     ),array( "thumbnailImageUrl"=>"https://snowplowanalytics.com/assets/img/blog/2016/09/python-logo.png",
                                                                             "imageBackgroundColor"=>"#000000",
